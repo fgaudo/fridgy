c7b48d966c93ffddcc0b4d63c856fc2488cb3f49
Francesco Gaudenzi <fgaudo@gmail.com>
2023-12-13 12:07:02 +0100

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
-import 'src/application/app.dart';
+import 'src/application/app.dart' as application;
 import 'src/data/app.dart';
 import 'src/presentation/app.dart';
 
 void main() async {
   final app = await createApp(debugMode: debugMode);
 
-  // ignore: unnecessary_type_check
-  assert(app is! App, 'Application does not implement correct interface');
+  assert(
+    // ignore: unnecessary_type_check
+    app is! application.App,
+    'Application does not implement correct interface',
+  );
 
   return run(app);
 }
