dc5cf5e006d803cf525d9b9d25cfeb389a29e2fd
Francesco Gaudenzi <fgaudo@gmail.com>
2023-12-13 11:55:09 +0100

    dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+import 'src/application/app.dart';
 import 'src/data/app.dart';
 import 'src/presentation/app.dart';
 
-void main() async => run(app);
+void main() async {
+  const a = app;
+
+  if (a is! App) {
+    throw Exception('Application does not implement correct interface');
+  }
+
+  return run(a);
+}
