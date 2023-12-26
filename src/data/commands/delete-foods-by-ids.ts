4f32f00e0b97dc7a63cbb58a7fa03d12d18efeb3
Francesco Gaudenzi <fgaudo@gmail.com>
2023-12-26 21:58:11 +0100

    dev

--- src/data/commands/delete-foods-by-ids.ts
+++ src/data/commands/delete-foods-by-ids.ts
@@ -1,6 +1,38 @@
-import { reader as R, taskEither as TE } from 'fp-ts'
+import { SQLiteDBConnection, capSQLiteSet } from '@capacitor-community/sqlite'
+import {
+	reader as R,
+	readonlyArray as ROA,
+	readonlySet as ROS,
+	readerTaskEither as RTE
+} from 'fp-ts'
+import { pipe } from 'fp-ts/function'
+import { tryCatch } from 'fp-ts/lib/TaskEither'
+import { Ord } from 'fp-ts/string'
 
 import { DeleteFoodsByIdsWithDeps } from '@/app/commands/delete-foods-by-ids'
 
-export const deleteFoodsByIds: DeleteFoodsByIdsWithDeps<unknown> = () =>
-	R.of(TE.right(undefined))
+type Deps = {
+	db: SQLiteDBConnection
+}
+
+export const deleteFoodsByIds: DeleteFoodsByIdsWithDeps<Deps> = idSet =>
+	pipe(
+		R.of(idSet),
+		R.map(ROS.toReadonlyArray(Ord)),
+		R.map(
+			ROA.map(i => ({
+				statement: 'DELETE * FROM foods where id=?',
+				values: [i] as any[]
+			}))
+		),
+		R.map(ROA.toArray), // the api wants a mutable array..
+		R.chain(
+			set =>
+				({ db }: Deps) =>
+					tryCatch(
+						() => db.executeSet(set),
+						e => (e instanceof Error ? e : Error('Unknown error'))
+					)
+		),
+		RTE.map(() => undefined)
+	)
--- src/data/commands/foods.ts
+++ src/data/commands/foods.ts
@@ -1,6 +1,20 @@
+import { SQLiteDBConnection } from '@capacitor-community/sqlite'
 import { reader as R } from 'fp-ts'
-import { of } from 'rxjs'
+import { flow, pipe } from 'fp-ts/lib/function'
+import { Observable, of, switchMap } from 'rxjs'
 
 import { FoodsWithDeps } from '@/app/commands/foods'
 
-export const foods: FoodsWithDeps<unknown> = R.of(of([]))
+type Deps = {
+	db: SQLiteDBConnection
+	events: Observable<void>
+}
+
+/** TODO */
+export const foods: FoodsWithDeps<Deps> = pipe(
+	R.asks((deps: Deps) => deps.events),
+	R.bindTo('events'),
+	R.bind('db', () => R.asks((deps: Deps) => deps.db)),
+	R.chain(({ db, events }) => pipe(events)),
+	switchMap(db => db.query('SELECT * FROM foods where id=?'))
+)
