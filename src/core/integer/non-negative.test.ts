8f01557a82be44a8b8995d56b8cb541c1849568b
Francesco Gaudenzi <fgaudo@gmail.com>
2024-09-23 13:37:52 +0200

    dev

---
+++ src/core/integer/non-negative.test.ts
@@ -0,0 +1,29 @@
+import { fc, test } from '@fast-check/vitest'
+import { assert, describe, expect } from 'vitest'
+
+import {
+	fromNumber,
+	unsafe_fromNumber,
+} from './non-negative.js'
+
+describe('integer', () => {
+	test.concurrent.prop([fc.nat()])(
+		'should be ok',
+		integer => {
+			const result = fromNumber(integer)
+			assert(
+				result._tag === 'Some',
+				'Could not parse number',
+			)
+			expect(result.value).toStrictEqual(integer)
+		},
+	)
+
+	test.concurrent.prop([fc.nat()])(
+		'should be ok',
+		integer => {
+			const number = unsafe_fromNumber(integer)
+			expect(number).toStrictEqual(integer)
+		},
+	)
+})
